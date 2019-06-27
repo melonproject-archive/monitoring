6e4130da1a414fc9ce5dd5b59489478322a84377
Sebastian Siemssen <fubhy@fubhy.com>
2019-06-27 15:37:01 +0200

    Fixing scraping query.

--- utils/useScrapingQuery.ts
+++ utils/useScrapingQuery.ts
@@ -1,10 +1,10 @@
 import * as R from 'ramda';
 import { useQuery } from '@apollo/react-hooks';
 import { DocumentNode } from 'graphql';
-import { useState } from 'react';
+import { useEffect } from 'react';
 
 type QueryPair = [DocumentNode, DocumentNode];
-type ProceedOrNotFn = (result: any, expected: number) => boolean;
+type ProceedOrNotFn = (result: any, expected) => boolean;
 
 const deepMerge = (a, b) => {
   if (Array.isArray(a) && Array.isArray(b)) {
@@ -18,7 +18,7 @@ const deepMerge = (a, b) => {
   return b;
 };
 
-export const proceedPaths = (paths: string[]) => (current: any, expected: number) => {
+export const proceedPaths = (paths: string[]) => (current: any, expected: number): boolean => {
   return R.path([...paths, 'length'], current) === expected;
 };
 
@@ -32,41 +32,39 @@ export function useScrapingQuery([query, more]: QueryPair, proceed: ProceedOrNot
   const result = useQuery(query, {
     ...props,
     variables,
-    onCompleted: (data: any) => {
-      const fetchMore = (merged: any, skip: number) => {
-        if (!proceed(merged, skip)) {
-          return false;
-        }
-
-        result.fetchMore({
-          query: more,
-          variables: {
-            ...variables,
-            skip,
-          },
-          updateQuery: (previous, options) => {
-            const output = deepMerge(previous, options.fetchMoreResult);
-            if (!fetchMore(output, skip + variables.limit)) {
-              setLoading(false);
-            }
-
-            return output;
-          },
-        });
-
-        return true;
-      };
-
-      if (!fetchMore(data, variables.skip + variables.limit)) {
-        setLoading(false);
-      }
-    },
   });
 
-  const [loading, setLoading] = useState(result.loading);
+  const fetchMore = (data, skip) => {
+    if (!proceed(data, skip)) {
+      return;
+    }
 
-  return {
-    ...result,
-    loading: result.error ? result.loading : loading,
+    result.fetchMore({
+      query: more,
+      variables: {
+        ...variables,
+        skip,
+      },
+      updateQuery: (previous, options) => {
+        const merged = deepMerge(previous, options.fetchMoreResult);
+        fetchMore(merged, skip + variables.limit);
+        return merged;
+      },
+    });
   };
+
+  // The length of the result is a good indicator for whether there are any
+  // results at all yet. The loading state is not helpful for that.
+  const filled = !!(result && result.data && Object.keys(result.data).length);
+
+  useEffect(() => {
+    if (!filled) {
+      return;
+    }
+
+    // Only after the query is initially filled, we continue.
+    fetchMore(result.data, variables.skip + variables.limit);
+  }, [filled]);
+
+  return result;
 }
