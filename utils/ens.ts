11900047caa65036a1c859c08e8056428fd513c3
Ivan Herger <ivan.herger@gmail.com>
2020-02-19 20:34:14 +0100

    Use new ens domains

--- utils/ens.ts
+++ utils/ens.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const fetchEnsAddresses = async (base: string = 'ETH') => {
-  const result = await axios('https://ens.melon.network/');
+  const result = await axios('https://ens-server-git-new-domains.melon-project.now.sh/');
   return result.data;
 };
