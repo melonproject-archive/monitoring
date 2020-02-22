a233f84f57267ec50339f1246e9ddb5945ee262f
Sebastian Siemssen <fubhy@fubhy.com>
2020-02-22 21:42:10 +0100

    Update ens.ts

--- utils/ens.ts
+++ utils/ens.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const fetchEnsAddresses = async (base: string = 'ETH') => {
-  const result = await axios('https://ens-server-git-new-domains.melon-project.now.sh/');
+  const result = await axios('https://ens.melon.network/');
   return result.data;
 };
